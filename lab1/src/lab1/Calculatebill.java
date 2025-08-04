23fd6580253d345375c90912469e11f8627f8b2d
SreejaNallapu <sreejanallapu27@gmail.com>
2025-08-04 20:57:00 +0530

    Update Calculatebill.java

--- lab1/src/lab1/Calculatebill.java
+++ lab1/src/lab1/Calculatebill.java
@@ -4,13 +4,14 @@ public class Calculatebill {
 	public static void main(String[] args) {
 		double bill,result;
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter the bill:");
+		System.out.println("Enter the bill:");
 		 bill=sc.nextDouble();
 		if(bill>1000) {
 			result=bill-(bill*0.1);
 		}else {
 			result=bill-(bill*0.05);
 		}
-        System.out.printf("Total Bill after discount: "+result);
+        System.out.println("Total Bill after discount: "+result);
     }
 }
+
